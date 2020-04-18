5d54a91c8ea780e3e2c55d5e372c4675537832c5
Kunal <kunaldandona009@gmail.com>
2020-04-18 15:15:28 -0700

    cod

--- functions.php
+++ functions.php
@@ -7,13 +7,13 @@ function stockie_child_local_enqueue_parent_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 
-function payment_gateway_disable_country($available_gateways)
-{
-    global $woocommerce;
-    if (isset($available_gateways['cod']) && $woocommerce->customer->get_country() != 'IN') {
-        unset($available_gateways['cod']);
+
+add_filter( 'woocommerce_available_payment_gateways', 'gateway_disable_country' );
+
+function gateway_disable_country( $available_gateways ) {
+    if ( is_admin() ) return $available_gateways;
+    if ( isset( $available_gateways['cod'] ) && WC()->customer->get_billing_country() != 'IN' ) {
+        unset( $available_gateways['cod'] );
     }
     return $available_gateways;
 }
-
-add_filter('woocommerce_available_payment_gateways', 'payment_gateway_disable_country');
