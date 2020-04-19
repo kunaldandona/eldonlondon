61477335ecdf9e903880eda5d7c40f74a862b4d9
Kunal <kunaldandona009@gmail.com>
2020-04-18 17:04:44 -0700

    checkout

--- functions.php
+++ functions.php
@@ -6,14 +6,3 @@ function stockie_child_local_enqueue_parent_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
-
-
-add_filter( 'woocommerce_available_payment_gateways', 'gateway_disable_country' );
-
-function gateway_disable_country( $available_gateways ) {
-    if ( is_admin() ) return $available_gateways;
-    if ( isset( $available_gateways['cod'] ) && WC()->customer->get_billing_country() != 'IN' ) {
-        unset( $available_gateways['cod'] );
-    }
-    return $available_gateways;
-}
